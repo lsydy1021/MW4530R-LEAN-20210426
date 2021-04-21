328bda0bdd45d2796bc9a01266fef0fa90d60d9a
lsydy1021 <59816384+lsydy1021@users.noreply.github.com>
2021-04-21 15:42:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 svn co https://github.com/Lienol/openwrt-luci/branches/19.07/themes/luci-theme-bootstrap-mod package/lean/luci-theme-bootstrap-mod #增加一个肤色
 git clone -b master https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns #luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns #smartdns进程编译脚本
+git clone https://github.com/xiaorouji/openwrt-passwall.git
